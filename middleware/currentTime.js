4eb957889a19378d4bd855761e0ee1533dbffa2a
heroprotagonist <jstepper91@gmail.com>
2017-08-05 14:47:44 -0400

    make current time function

--- middleware/currentTime.js
+++ middleware/currentTime.js
@@ -7,16 +7,21 @@ const cache = Cache()
 function currentTime (req, res, next) {
   log(`attempting to get current time from cache`)
   return cache.wrap('current-time', () => {
-      return `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+      return calculateCurrentTime()
     })
     .then(currentTime => {
-      res.currentTime = currentTime
+      res.currentTime =  currentTime
       next()
     })
     .catch(e => {
-      console.warn('current time cache error: ', e)
+      log('current time cache error: ', e)
+      res.currentTime = calculateCurrentTime()
       next()
     })
+}
+
+function calculateCurrentTime () {
+  return `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
 }
 
 module.exports = {
