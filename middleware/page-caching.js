33cdc5100686c9ca3439a303ce4d1d3b42ceda6a
heroprotagonist <jstepper91@gmail.com>
2017-08-05 13:00:26 -0400

    add cache and render middleware

---
+++ middleware/page-caching.js
@@ -0,0 +1,48 @@
+const Debug = require('debug')
+const Cache = require('../redis/cache')
+
+const log = Debug('pageCaching')
+const cache = Cache()
+
+module.exports = {
+  cachePage: (req, res, next) => {
+    try {
+      const cacheKey = req.originalUrl
+      log(`caching page for key: ${cacheKey}`)
+
+      cache.set(cacheKey, res.html, err => {
+        err ? next(err) : next()
+      })
+    } catch (e) {
+      console.warn(e)
+      next()
+    }
+  },
+
+  renderIfCached: (req, res, next) => {
+
+    const cacheKey = req.originalUrl
+    log(`checking page cache for key: ${cacheKey}`)
+
+    try {
+      cache.get(cacheKey, (err, html) => {
+        // html is null if not found in cache
+
+        if (err) {
+          // log error but continue request normally
+          log(`redis error fetching cached page: ${err}`)
+          return next()
+        } else if (html === null) {
+          log(`cache miss, continuing with request: ${req.originalUrl}`)
+          return next()
+        } else {
+          log(`rendering cached page: ${req.originalUrl}`)
+          res.send(html)
+        }
+      })
+    } catch (e) {
+      console.warn(e)
+      next()
+    }
+  }
+}
---
+++ middleware/render.js
@@ -0,0 +1,13 @@
+const pug = require('pug')
+const path = require('path')
+
+function render (req, res, next) {
+  res.html = pug.renderFile(path.join(__dirname, '..', 'views/index.pug'), {
+    timestamp: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+  })
+  next()
+}
+
+module.exports = {
+  render,
+}
