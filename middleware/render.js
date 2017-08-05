049d3e5c9da3735efad714d1d34bf7a56a76a07f
heroprotagonist <jstepper91@gmail.com>
2017-08-05 14:28:59 -0400

    add comment with no cache date

--- middleware/render.js
+++ middleware/render.js
@@ -3,7 +3,8 @@ const path = require('path')
 
 function render (req, res, next) {
   res.html = pug.renderFile(path.join(__dirname, '..', 'views/index.pug'), {
-    timestamp: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+    timestamp: res.currentTime
+    // timestamp: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
   })
   next()
 }
