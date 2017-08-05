6791dc66d9ca0ec88952e4e23ad247ee0403da33
heroprotagonist <jstepper91@gmail.com>
2017-08-05 12:30:20 -0400

    lint file

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan('tiny'))
-app.set("view engine", "pug")
+app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.render('index', {
     timestamp: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
   })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, () => {
+  console.log('Server listening on port 3000...')
 })
